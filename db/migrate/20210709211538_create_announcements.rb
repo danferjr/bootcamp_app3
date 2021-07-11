8e449433ff7a81dad27300ccbb4d6735d264e233
danferjr <danielmoral@unicauca.edu.co>
2021-07-11 22:48:33 +0000

    Listo la entrada de usuario

---
+++ db/migrate/20210709211538_create_announcements.rb
@@ -0,0 +1,11 @@
+class CreateAnnouncements < ActiveRecord::Migration[6.1]
+  def change
+    create_table :announcements do |t|
+      t.string :content
+      t.string :expire
+      t.string :visto
+
+      t.timestamps
+    end
+  end
+end
